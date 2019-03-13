958715d6bc709b84648359bd68787fbf5e74ec16
Camille-Dev <camille.theil@gmail.com>
2019-03-13 06:35:24 +0100

    patch: change csv

--- index.js
+++ index.js
@@ -6,18 +6,20 @@ var stream = fs.createReadStream("./test.csv");
 let array_id = "";
 let id = "";
 let publish = "";
+let name = "";
 
 // 1. Read the CSV and get the "Title" value
 function ReadCSV () {
   csv
-    .fromStream(stream, {headers : ["Type", "Id", "Publish"]})
+    .fromStream(stream, {headers : ["Type", "Title", "Id", "Publish"]})
     .on("data", function(data){
-        console.log('Info - ID :', data.Id)
-        console.log('Info - Will be publish ? - ', data.Publish)
+        // console.log('Info - ID :', data.Id)
+        // console.log('Info - Will be publish ? - ', data.Publish)
         id = data.Id;
         publish = data.Publish;
+        title = data.Title;
         array_id += [data.Id + ";"];
-
+        console.log("Info - 1 personnage found : ", data.Title)
         GetSomeShit();
         UpdateSomeShit();
       })
@@ -28,14 +30,12 @@ function ReadCSV () {
       });
 
   }
-
-
 // 2. Make a get request to get all ID in an array
 function GetSomeShit () {
   // Make a request
   axios.get('http://localhost:3000/personnages/' + id )
     .then(function (response) {
-      console.log("Info - 1 personnage found : ", response.data.title)
+
     })
     .catch(function (error) {
       console.log("Error - 0 personnage found with id : ", id)
@@ -48,7 +48,9 @@ function GetSomeShit () {
 // 3. Update publish parameter for personnages
 function UpdateSomeShit () {
   axios.put('http://localhost:3000/personnages/' + id, {
+    title: title,
     publish: publish
+
   })
   .then(response => {
     console.log("Info - Updating " + response.data.title + " with publish=" + publish)
